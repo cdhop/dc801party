37c70392ae04c83a425a0c3747343cfc93cc2d99
Christopher Hopkins <cdhop@Christophers-MacBook-Pro-3.local>
2017-07-24 13:16:14 -0600

    removed unneeded gems

--- Gemfile
+++ Gemfile
@@ -3,6 +3,5 @@ source 'https://rubygems.org'
 gem "rack"
 gem "unicorn"
 gem "sinatra"
-gem "sanitize"
 gem "curb"
 
--- config/config.rb
+++ config/config.rb
@@ -1,10 +1,7 @@
 require 'sinatra'
-require 'sanitize'
-require 'carmen'
 require 'curb'
 require 'json'
 require 'yaml'
-require 'securerandom'
 
 Dir.glob( File.expand_path('lib/*.rb') ).each do |lib_file|
   require lib_file
